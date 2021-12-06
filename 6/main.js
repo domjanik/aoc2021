703f5cd06a4aee2c1152fb658c95cefb59430b80
djanik <dj@kazan.com.pl>
2021-12-06 07:53:34 +0100

    cleanup day 6

--- 6/main.js
+++ 6/main.js
@@ -2,23 +2,40 @@ const fs = require('fs');
 const fishes = fs.readFileSync('input.txt', 'utf8')
 .split(',').map(fish => Number(fish));
 
-function simulateFishLifetime(input, days) {
-    let tempFishes = [...input]
-    for(let i = 0; i < days; i++) {
-        tempFishes = tempFishes.map((fish) => {
-            if(fish != 0) {
-                return fish = fish - 1;
-            } else {
-                return fish;
-            }
-        })
-        tempFishes.filter(fish => fish === 0).forEach(fish => {
-            fish = 6;
-            tempFishes.push(8);
-        })
-    }
-
-    return tempFishes.length;
+let fishDays = {
+    0: 0,
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0,
+    6: 0,
+    7: 0,
+    8: 0,
+    freshFishes: 0,
 }
 
-console.log(simulateFishLifetime(fishes, 80))
+function init(input) {
+    input.forEach(fish => fishDays[fish]++);
+}
+
+function simulateFishLifetime(input, days) {
+    init(input)
+    for(let day = 0; day < days; day++) {
+        for(let fishAge = 0; fishAge< 9; fishAge++) {
+            if(fishAge == 0) {
+                fishDays.freshFishes = fishDays[0];
+            } else {
+                fishDays[fishAge-1] = fishDays[fishAge];
+            }
+        }
+        fishDays[8] = fishDays.freshFishes;
+        fishDays[6] += fishDays.freshFishes;
+        fishDays.freshFishes = 0;
+    }
+
+    return Object.keys(fishDays).reduce((previousValue, currentValue) => previousValue + fishDays[currentValue], 0)
+}
+
+console.log(`After 80 days there will be : ${simulateFishLifetime(fishes, 80)} fishes`)
+console.log(`After 80 days tehre will be : ${simulateFishLifetime(fishes, 256)} fishes`)
